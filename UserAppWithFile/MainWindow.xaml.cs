5fb3a4cd9b0edee50c0b3b1eb4e35f478af4b911
bytda <bytdae@gmail.com>
2022-05-06 00:49:20 +0300

    first commit

---
+++ UserAppWithFile/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace UserAppWithFile
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string path = @"C:\Users\forda\source\repos\UserAppWithFile\UserAppWithFile\data.txt";
+
+
+            //File.Create(path).Close();
+            //File.WriteAllText(path, "Calam");
+            //using (FileStream file = new FileStream(path, FileMode.Append))
+            using (StreamWriter stream = new StreamWriter(path, true)) {
+                if (login.Text != "" && pass.Text != "")
+                {
+                    stream.WriteLine(login.Text.ToLower() + "|" + pass.Text.ToLower());
+                    Window1 window1 = new Window1();
+                    window1.Show();
+                    Close();
+                }
+                else MessageBox.Show("Заполните поля");
+            }
+            
+
+
+
+        }
+        
+        
+    }
+}
---
+++ UserAppWithFile/Window1.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace UserAppWithFile
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        
+        
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        string path = @"C:\Users\forda\source\repos\UserAppWithFile\UserAppWithFile\data.txt";
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+            string loginAth = login.Text.ToLower();
+            string passAth = pass.Text.ToLower();
+            string dataAth = loginAth + "|" + passAth;
+
+            using (StreamReader read = new StreamReader(path))
+            {   //string dataFile = read.ReadToEnd();
+                //if (dataFile.Contains(dataAth)) MessageBox.Show("Авторизованы");
+                //else MessageBox.Show("Неверно");
+                int i = 0;
+                string[] dataFile = new string[33];
+                
+                int match = 0;
+                while (!read.EndOfStream)
+                {
+                    dataFile[i] = read.ReadLine();
+
+                    if (dataFile[i] == dataAth) match++;
+                    
+                    
+                    i++;
+                    
+                }
+
+                if (match != 0) MessageBox.Show("Авторизованы");
+                else MessageBox.Show("Пользователь не найден");
+
+
+            }
+            
+            
+
+              
+        }
+    }
+}
